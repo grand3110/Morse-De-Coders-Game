c01d1ce5a906b037150c3bbe9b67ab5e5267301e
grand3110 <39327873+grand3110@users.noreply.github.com>
2018-12-11 08:14:24 +0800

    final

--- work/planAhead/new mojo game v12/new mojo game v12.srcs/sources_1/imports/verilog/input_capture_16.v
+++ work/planAhead/new mojo game v12/new mojo game v12.srcs/sources_1/imports/verilog/input_capture_16.v
@@ -15,7 +15,7 @@ module input_capture_16 (
   
   
   
-  reg [4:0] M_button_col_cycler_d, M_button_col_cycler_q = 1'h0;
+  reg [5:0] M_button_col_cycler_d, M_button_col_cycler_q = 1'h0;
   reg [23:0] M_counter_d, M_counter_q = 1'h0;
   
   wire [5-1:0] M_bm_char;
@@ -35,13 +35,13 @@ module input_capture_16 (
     guessled = 2'h3;
     M_bm_button_col = M_button_col_cycler_q;
     M_bm_button_row = button_row;
+    button_col = M_button_col_cycler_q;
     if (M_button_col_cycler_q == 1'h0) begin
       M_button_col_cycler_d = 1'h1;
     end
     if (button_row != 1'h0) begin
       guessled = 2'h0;
     end
-    button_col = M_button_col_cycler_q;
     M_counter_d = M_counter_q + 1'h1;
     if (button_row == 1'h0 && M_counter_q[20+0-:1] == 1'h1) begin
       M_button_col_cycler_d = M_button_col_cycler_q << 1'h1;
